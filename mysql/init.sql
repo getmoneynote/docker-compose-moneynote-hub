715803361bc4d3e8bfbde12357e2dd5c22a171c3
markliu2013 <markliu2013@gmail.com>
2023-06-15 23:03:07 +0800

    feat

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,2 +1,2 @@
-CREATE DATABASE moneywhere CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS moneywhere CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE moneywhere;
